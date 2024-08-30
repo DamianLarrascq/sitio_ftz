6388af61a74541040e5eb85b2eb91002025252c7
Damian Larrascq <damian.larrascq@gmail.com>
2024-08-29 23:55:35 -0300

    WIP login

--- app/main.py
+++ app/main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
-from app.routes.routes import users_router
+from app.routes.routes import users_router, auth_router
 import uvicorn
-from fastapi.security import OAuth2PasswordBearer
 
 app = FastAPI()
 
 app.include_router(users_router)
-
-# oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+app.include_router(auth_router)
 
 
 @users_router.get("/")
@@ -15,4 +13,4 @@ def get_home():
     return {'message': 'algo'}
 
 
-uvicorn.run(app, host="127.0.0.1", port=8000)
+uvicorn.run(app, host="127.0.0.1", port=8001)
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,10 +1,9 @@
 from typing import Annotated
-
 from fastapi import Depends, APIRouter, HTTPException
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from starlette.requests import Request
 from app.controllers import crud
-# from app.main import oauth2_scheme
 from app.models import schemas
 from app.models.base import SessionLocal
 
@@ -17,17 +16,27 @@ def get_db():
         db.close()
 
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+
+
 users_router = APIRouter(
     # prefix="/items",
     tags=["users"],
-    # dependencies=[Depends(get_token_header)],
+    dependencies=[Depends(oauth2_scheme)],
+    # responses={404: {"description": "Not found"}},
+)
+auth_router = APIRouter(
+    # prefix="/items",
+    tags=["auth"],
+    # dependencies=[Depends(oauth2_scheme)],
     # responses={404: {"description": "Not found"}},
 )
 
 
-# @users_router.get("/users/getuser")
-# async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
-#     return {"token": token}
+@auth_router.post("/token")
+async def get_token(request: Request, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    print(form_data)
+    return form_data
 
 
 @users_router.get("/users/", response_model=list[schemas.UserGet])
@@ -37,7 +46,7 @@ async def get_users(request: Request, skip: int = 0, limit: int = 100, db: Sessi
 
 
 @users_router.get("/users/{user_id}")
-async def get_users_id(request: Request, user_id: int):
+async def get_users_id(request: Request, user_id: int, token=Depends(oauth2_scheme)):
     return user_id
 
 
@@ -52,12 +61,3 @@ async def post_users(request: Request, user: schemas.UserCreate, db: Session = D
 
     return result
 
-
-@users_router.post("/users/login")
-async def users_login():
-    pass
-
-
-@users_router.post("/users/logout")
-async def users_logout():
-    pass
